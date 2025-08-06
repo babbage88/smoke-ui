7ae9aeff397df9db4bb687286ad8c5edd2c9541a
Justin Trahan <justin@trahan.dev>
2025-08-05 19:56:39 -0500

    Modified shadcn button for metter text color in dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "./components/ui/button";
+import { Label } from "@/components/ui/label"
+
 import "./App.css";
 
 function App() {
@@ -14,18 +17,13 @@ function App() {
             <ModeToggle />
           </header>
         </div>
-        <h1>Smoke Shop</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test HMR
-          </p>
+        
+        <div className="fixed top-1 left-2 p-4 card">
+          <h1>Smoke Shop</h1>
+          <Button size="default" onClick={() => setCount((count) => count + 1)}>
+            <Label>Primary count is {count}</Label>
+          </Button>
         </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
       </ThemeProvider>
     </>
   );
